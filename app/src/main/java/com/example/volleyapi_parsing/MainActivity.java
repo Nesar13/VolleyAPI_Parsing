699f307eb988654beea83a04699066e8afe38e30
Nesar Ahmed <nesar13@hotmail.com>
2020-01-11 00:32:55 -0500

    json request

--- app/src/main/java/com/example/volleyapi_parsing/MainActivity.java
+++ app/src/main/java/com/example/volleyapi_parsing/MainActivity.java
@@ -3,13 +3,39 @@ package com.example.volleyapi_parsing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    //volley api is not compatible with android api 29
+
+    //https://jsonplaceholder.typicode.com/todos/1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://jsonplaceholder.typicode.com/todos/1", null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("JSON: ", "onResponse: " +response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        })
     }
 }
